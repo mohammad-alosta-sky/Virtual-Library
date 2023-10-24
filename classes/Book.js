7244f8c8ee147bed54665e8021f677c13df0b24d
Mohammad Al osta <mohammad.alosta@sky.uk>
2023-10-24 14:31:20 +0100

    Book and Media Classes are done

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,21 @@
 // import the Media class:
-
+const Media = require('./Media')
 // create your Book class:
+
+class Book extends Media {
+    constructor(title, year, genre, auther, numPages, rating) {
+        super(title, year, genre);
+        this.auther = auther;
+        this.numPages = numPages;
+        this.rating = rating;
+
+    }
+
+    summary() {
+        return `Title: ${this.title}, Author: ${this.auther}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+}
+
 
 // don't change below
 module.exports = Book;
